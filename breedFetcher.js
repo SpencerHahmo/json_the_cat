da9ae9432f3a8c570d4be4a313699f99634c5159
Spencer Hahmo <schahmo@gmail.com>
2023-02-17 18:48:39 -0700

    Refatored breedFetcher.js to only return the description/ error to index.js via callback

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,24 +1,23 @@
 const request = require('request');
 
-const args = process.argv.slice(2);
-const catBreed = args[0];
+// const args = process.argv.slice(2);
+// const breedName = args[0];
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${catBreed}`, (error, response, body) => {
-  // If there is an issue with the url
-  if (error) {
-    console.log(`${error.message}\nPlease enter the url correctly (i.e http://example.domain)`);
-    // Ends the program
-    return;
-  }
+const fetchBreedDescription = (breedName, callback) => {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
     
-  const data = JSON.parse(body);
-  if (data[0] !== undefined) {
-    console.log(data[0].description);
-    return;
-  } if (catBreed === undefined) {
-    console.log(`No cat breed was entered ...`);
-    return;
-  } else {
-    console.log(`There was no data on the breed you requested (${catBreed}).\nDid you enter the breed correctly?`);
-  }
-});
+    const data = JSON.parse(body);
+    if (data.length === 0) {
+      callback("Breed not found", null);
+      return;
+    }
+        
+    callback(null, data[0].description);
+  });
+};
+
+module.exports = { fetchBreedDescription };
